7329f051d854def6af45c745ac4fc29e02e62326
enlorik <99548776+enlorik@users.noreply.github.com>
2025-12-04 13:40:09 +0100

    Add findByUser method to PasswordResetTokenRepository
    Added method to find PasswordResetToken by User.

--- src/main/java/com/empress/usermanagementapi/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/empress/usermanagementapi/repository/PasswordResetTokenRepository.java
@@ -1,9 +1,15 @@
 package com.empress.usermanagementapi.repository;
 
 import com.empress.usermanagementapi.entity.PasswordResetToken;
+import com.empress.usermanagementapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
     Optional<PasswordResetToken> findByToken(String token);
+
+    // new: so we can overwrite the same row for the same user
+    Optional<PasswordResetToken> findByUser(User user);
 }
