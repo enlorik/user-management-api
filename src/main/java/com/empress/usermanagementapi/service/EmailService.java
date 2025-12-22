5f7917361c7ede4331be10081c4e4a3831c78960
enlorik <99548776+enlorik@users.noreply.github.com>
2025-12-22 02:14:13 +0100

    Enhance EmailService with default 'from' address
    Added default 'from' address for password reset emails.

--- src/main/java/com/empress/usermanagementapi/service/EmailService.java
+++ src/main/java/com/empress/usermanagementapi/service/EmailService.java
@@ -1,18 +1,30 @@
 package com.empress.usermanagementapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     @Autowired
     private JavaMailSender mailSender;
+
+    // use whatever is configured as spring.mail.username
+    @Value("${spring.mail.username}")
+    private String defaultFrom;
 
     public void sendPasswordResetEmail(String to, String resetLink) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(to);
+
+        // set a valid "from" address (important for Gmail)
+        if (defaultFrom != null && !defaultFrom.isBlank()) {
+            msg.setFrom(defaultFrom);
+        }
+
         msg.setSubject("Password Reset Request");
         msg.setText(
             "You requested to reset your password.\n\n" +
@@ -20,6 +32,9 @@ public class EmailService {
             resetLink + "\n\n" +
             "If you didn't request this, you can ignore this email."
         );
+
+        System.out.println("[MAIL] Sending password reset to " + to);
         mailSender.send(msg);
+        System.out.println("[MAIL] Password reset email sent (or queued) successfully");
     }
 }
